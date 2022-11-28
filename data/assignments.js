83ef57b1613a4914dc51b9583490c52a76554b40
planit <planit@tanjeffreyz.github.io>
2022-11-28 19:31:35 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -801,6 +801,13 @@ assignments = {
       "dueDate": "2022-11-22T23:59:00",
       "link": "https://www.gradescope.com/courses/432158",
       "submitted": false
+    },
+    {
+      "title": "Assignment 6 (Late or DSP)",
+      "course": "EPS 82-LEC-001",
+      "dueDate": "2022-11-30T23:59:00",
+      "link": "https://www.gradescope.com/courses/432158/assignments/2464702/submissions/new",
+      "submitted": false
     }
   ],
   "INDENG 151 ": [
