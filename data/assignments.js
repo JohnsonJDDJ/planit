9eb45a6ba70c5e4b1b4cd675f8fe39ba286272bd
planit <planit@tanjeffreyz.github.io>
2022-12-01 08:40:15 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -820,7 +820,7 @@ assignments = {
       "title": "Assignment 6 (Late or DSP)",
       "course": "EPS 82-LEC-001",
       "dueDate": "2022-11-30T23:59:00",
-      "link": "https://www.gradescope.com/courses/432158/assignments/2464702/submissions/new",
+      "link": "https://www.gradescope.com/courses/432158",
       "submitted": false
     }
   ],
