78070e63f69fec13c3f680ff4a7567e1e59ca407
planit <planit@tanjeffreyz.github.io>
2022-12-14 08:37:44 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -706,7 +706,7 @@ assignments = {
       "title": "Lab 9 Self-grade",
       "course": "EECS 126",
       "dueDate": "2022-12-13T23:00:00",
-      "link": "https://www.gradescope.com/courses/412456/assignments/2466704/submissions/new",
+      "link": "https://www.gradescope.com/courses/412456",
       "submitted": false
     }
   ],
