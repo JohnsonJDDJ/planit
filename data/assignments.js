7e04444713a36e7e75cb3c4be31d9904ef268999
planit <planit@tanjeffreyz.github.io>
2023-01-24 21:31:27 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -1,5 +1,13 @@
 assignments = {
   "Econ 141": [],
   "INFO 159/259": [],
-  "Physics/Astron C161": []
+  "Physics/Astron C161": [
+    {
+      "title": "Problem Set #1",
+      "course": "Physics/Astron C161",
+      "dueDate": "2023-01-27T23:59:00",
+      "link": "https://www.gradescope.com/courses/500377",
+      "submitted": false
+    }
+  ]
 };
