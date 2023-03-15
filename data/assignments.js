560499ab08bf4e328de9506727ac8025ec2b8eca
planit <planit@tanjeffreyz.github.io>
2023-03-15 05:31:30 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -80,6 +80,13 @@ assignments = {
       "dueDate": "2023-03-15T02:00:00",
       "link": "https://www.gradescope.com/courses/501270/assignments/2727242/submissions/168583577",
       "submitted": true
+    },
+    {
+      "title": "HW5 (ipynb)",
+      "course": "INFO 159/259",
+      "dueDate": "2023-03-15T02:00:00",
+      "link": "https://www.gradescope.com/courses/501270",
+      "submitted": false
     }
   ],
   "Physics/Astron C161": [
