a5f222d4c2b46cf9424a0257918c79f903f755a9
planit <planit@tanjeffreyz.github.io>
2023-04-12 15:31:35 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -112,7 +112,7 @@ assignments = {
     {
       "title": "HW6",
       "course": "INFO 159/259",
-      "dueDate": "2023-04-12T23:59:00",
+      "dueDate": "2023-04-13T02:00:00",
       "link": "https://www.gradescope.com/courses/501270/assignments/2804117/submissions/173740967",
       "submitted": true
     }
